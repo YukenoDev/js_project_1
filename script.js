a94bc2652d2c43e09e0cd2ffdfcd971839ef9091
Beksultan <zxc.beksultan@gmail.com>
2022-07-15 22:07:55 +0300

    3 practice lesson

--- script.js
+++ script.js
@@ -1,47 +1,31 @@
-'use strict';
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
 
-console.log(NaN || 2 || undefined); // 2
+// for (let i = 0; i < arr.length; i++) {
+// 	result[i] = arr[i];
+// }
 
-console.log(NaN && 2 && undefined); //NaN
-
-console.log(1 && 2 && 3); //3
-
-console.log(!1 && 2 || !3); //false
-
-console.log(25 || null && !3); //25
-
-console.log(NaN || null || !3 || undefined || 5); //5
-
-console.log(NaN || null && !3 && undefined || 5); //5
-
-console.log(5 === 5 && 3 > 1 || 5); //true
+// console.log(result);
 
 
-// const hamburger = 3;
-// const fries = 3;
-// const cola = 0;
-// const nuggets = 2;
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
 
-// if (hamburger === 3 && cola || fries === 3 && nuggets) {
-// 	console.log('Done!');
-// } //Да
+// for (let i = 0; i < data.length; i++) {
+// 	if (typeof (data[i]) === 'number') {
+// 		data[i] *= 2;
+// 	} else if (typeof (data[i]) === 'string') {
+// 		data[i] += ' - done';
+// 	}
+// }
 
-
-// let hamburger;
-// const fries = NaN;
-// const cola = 0;
-// const nuggets = 2;
-
-// if (hamburger || cola || fries === 3 || nuggets) {
-// 	console.log('Done!');
-// } //Да
+// console.log(data);
 
 
-let hamburger;
-const fries = NaN;
-const cola = 0;
-const nuggets = 2;
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+const result = [];
 
-if (hamburger && cola || fries === 3 && nuggets) {
-	console.log('Done!');
-} // Нет
+for (let i = 0; i < data.length; i++) {
+	result[i] = data[data.length - (i + 1)];
+}
+
+console.log(result);
